3bbbb27a3b36f10e310fa6c554e2e13da9545e21
ganeshphate1992 <ganeshphate1992@gmail.com>
2024-01-04 23:32:17 +0530

    Update

--- src/Header/header.js
+++ src/Header/header.js
@@ -1,8 +1,10 @@
-import React from "react";
-import styles from './header.module.css'
-export const Header=()=>{
-    return <div className={`text-center bg-primary text-while ${styles.header}`}>
-     End to End application
+import React from 'react'
+import styles from './Header.module.css'
+export const Header = () => {
+  return (
+    <div className={`text-center bg-primary text-white ${styles.header}`}>
+      End to End application
     </div>
+  )
 }
 
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,10 @@
 import Image from 'next/image'
 import styles from './page.module.css'
-
+import { Login } from '@/Login'
 export default function Home() {
   return (    
-        <div class={styles.clr}> 
-          <h1> Login Page Section </h1>
+        <div> 
+           <Login />
         </div>     
   )
 }
