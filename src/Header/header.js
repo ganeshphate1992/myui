a6ef86bcb824f77cec162b94d978f3333b7d037d
ganeshphate1992 <ganeshphate1992@gmail.com>
2024-01-11 16:44:59 +0530

    latest

--- src/Header/header.js
+++ src/Header/header.js
@@ -2,7 +2,7 @@ import React from 'react'
 import styles from './Header.module.css'
 export const Header = () => {
   return (
-    <div className={`text-center bg-primary text-white ${styles.header}`}>
+    <div className={`text-center bg-primary text-white ${styles.header1}`}>
       End to End application
     </div>
   )
